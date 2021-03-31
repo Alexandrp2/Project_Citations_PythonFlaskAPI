aabaf500661c6f45c0e1d2cb90ab543e4ced424d
Alexandrp2 <undercoverword@gmail.com>
2021-03-31 11:38:29 +0200

    update

--- flask_api_mongo_citations.py
+++ flask_api_mongo_citations.py
@@ -1,20 +1,23 @@
 from flask import Flask, request, Response, jsonify, json
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 from bson.json_util import dumps
 from bson import ObjectId
+import re
+
 '''
 Pour la gestion du CORS
-
 pip install -U flask-cors
-
-app = Flask(__name__)
-CORS(app)
-
 '''
 
 app = Flask(__name__)
-# app.config['MONGO_URI'] = "mongodb://localhost:27017/citations-app"
-app.config['MONGO_URI'] = "mongodb+srv://citationDbUser:citationDbPassword@cluster0.ooo2r.mongodb.net/citations-app?retryWrites=true&w=majority"
+
+# Autorise le CORS pour toute l'application (toutes les routes)
+CORS(app)
+
+# Configuration pour utilisation d'une base MongoDB locale ou distante (sur le cloud MongoDB Atlas)
+app.config['MONGO_URI'] = "mongodb://localhost:27017/citations-app"
+# app.config['MONGO_URI'] = "mongodb+srv://citationDbUser:citationDbPassword@cluster0.ooo2r.mongodb.net/citations-app?retryWrites=true&w=majority"
 
 mongo = PyMongo(app)
 
@@ -23,6 +26,13 @@ def test():
     return Response(response=json.dumps({"Status": "UP"}),
                     status=200,
                     mimetype='application/json')
+
+
+'''
+
+ROUTES CITATIONS
+
+'''
 
 
 @app.route('/citations', methods=['GET'])
@@ -41,6 +51,32 @@ def getCitationById(id):
     return Response(response,
                     status=200,
                     mimetype='application/json')
+
+
+@app.route('/citations/auteur/<auteur>', methods=['POST'])
+def getCitationsByAuthor(auteur):
+    #citations = mongo.db.citations.find({"author":{"$regex": '/aPollinaiRE/', "$options" :'i'}})
+    citations = mongo.db.citations.find({"author": "Guillaume Apollinaire"})
+    response = dumps(citations)
+    return Response(response,
+                    status=200,
+                    mimetype='application/json')
+
+'''
+
+ROUTES MON ESPACE
+
+'''
+
+
+
+
+'''
+
+ROUTES STATISTIQUES
+
+'''
+
 
 
 if __name__ == "__main__":
