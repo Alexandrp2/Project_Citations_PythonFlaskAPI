e5479f3578c2dc2494260d9fcbc4299c80140759
Alexandrp2 <undercoverword@gmail.com>
2021-04-01 21:53:06 +0200

    update

--- flask_api_mongo_citations.py
+++ flask_api_mongo_citations.py
@@ -53,20 +53,97 @@ def getCitationById(id):
                     mimetype='application/json')
 
 
-@app.route('/citations/auteur/<auteur>', methods=['POST'])
-def getCitationsByAuthor(auteur):
-    #citations = mongo.db.citations.find({"author":{"$regex": '/aPollinaiRE/', "$options" :'i'}})
-    citations = mongo.db.citations.find({"author": "Guillaume Apollinaire"})
+@app.route('/citations/recherche/auteur', methods=['POST'])
+def getCitationsByAuthor():
+    requestBoby = request.get_json()
+    authorToSearch = requestBoby.get('authorToSearch')
+    #autor = 'aU'
+    citations = mongo.db.citations.find(
+        {"author":{"$regex": authorToSearch +'.*', "$options" :'i'}})
     response = dumps(citations)
     return Response(response,
                     status=200,
                     mimetype='application/json')
+
+@app.route('/citations/recherche/string', methods=['POST'])
+def getCitationsByCitation():
+    requestBoby = request.get_json()
+    stringToSearch = requestBoby.get('stringToSearch')
+    citations = mongo.db.citations.find(
+        {"citation":{"$regex": stringToSearch +'.*', "$options" :'i'}})
+    response = dumps(citations)
+    return Response(response,
+                    status=200,
+                    mimetype='application/json')
+
+@app.route('/citations/recherche/auteuretstring', methods=['POST'])
+def getCitationsByAuthorAndString():
+    requestBoby = request.get_json()
+    authorToSearch = requestBoby.get('authorToSearch')
+    stringToSearch = requestBoby.get('stringToSearch')
+    citations = mongo.db.citations.find(
+        {"author":{"$regex": authorToSearch +'.*', "$options" :'i'}, 
+        "citation":{"$regex": stringToSearch +'.*', "$options" :'i'}})
+    response = dumps(citations)
+    return Response(response,
+                    status=200,
+                    mimetype='application/json')
+
 
 '''
 
 ROUTES MON ESPACE
 
 '''
+
+@app.route('/citation/favoris/mesCitations', methods=['POST'])
+def MesCitationFavByIPoster():
+    requestBoby = request.get_json()
+    poster = requestBoby.get('Poster')
+    citation = mongo.db.citations.find({'likers.login':poster})
+    response = dumps(citation)
+    return Response(response,
+                    status=200,
+                    mimetype='application/json')
+
+
+@app.route('/citation/delete/macitation', methods=['DELETE'])
+def deleteCitationByIdAndPoster():
+    requestBoby = request.get_json()
+    citationId = ObjectId(requestBoby.get('citationId'))
+    poster = requestBoby.get('Poster')
+    citation = mongo.db.citations.remove({"_id": citationId,"citationPoster":poster})
+    response = dumps(citation)
+    return Response(response,
+                    status=200,
+                    mimetype='application/json')
+
+
+@app.route('/citation/favoris/add', methods=['POST'])
+def addCitationByIdAndPosterToFav():
+    requestBoby = request.get_json()
+    citationId = ObjectId(requestBoby.get('citationId'))
+    poster = requestBoby.get('Poster')
+    citation = mongo.db.citations.update(
+   { '_id': citationId}, {'$inc': {"savedInFavorites": 1, }, '$addToSet':  {'likers' :{'name': poster,'login': poster }, } })
+    response = dumps(citation)
+    return Response(response,
+                    status=200,
+                    mimetype='application/json')
+
+
+@app.route('/citation/favoris/del', methods=['POST'])
+def deleteCitationByIdAndPosterToFav():
+    requestBoby = request.get_json()
+    citationId = ObjectId(requestBoby.get('citationId'))
+    poster = requestBoby.get('Poster')
+    citation = mongo.db.citations.update(
+   { '_id': citationId},  {'$inc':  {   "savedInFavorites": -1,  }, '$pull':  {'likers' :{'name':poster,'login':poster }, } })
+    response = dumps(citation)
+    return Response(response,
+                    status=200,
+                    mimetype='application/json')
+
 
 
 
@@ -77,6 +154,33 @@ ROUTES STATISTIQUES
 
 '''
 
+@app.route('/citation/stats/<stat>', methods=['GET'])
+def statCitations(stat):
+    
+    if stat == 'top3citation':
+        citation = mongo.db.citations.find({}, { 'savedInFavorites': 1, 'citation': 1 }).sort([( 'savedInFavorites', -1 )]).limit(3)
+
+    elif stat =='bestlogin':
+        citation = mongo.db.citations.aggregate( [ { '$unwind': "$citationPoster" }, { '$sortByCount': "$citationPoster" },{ '$sort': {"count": -1 }}, { '$limit': 1 }   ])
+
+    elif stat =='topquotedauthor':
+        citation = mongo.db.citations.aggregate( [ { '$unwind': "$author" }, { '$sortByCount': "$author" },{ '$sort': {"count": -1 }}, { '$limit': 1 }   ])
+
+
+    elif stat =='favouriteauthor':
+        citation = mongo.db.citations.aggregate( [{ '$project':{   'author': 1, 'numberOfLikers': { '$size': "$likers" }  } },   
+        { '$match': { 'author': { '$ne': None } } }, 
+        { '$group' :   {   "_id" : "$author",  " nbLikers": {'$sum': "$numberOfLikers"}   }  },
+        { '$sort': {" nbLikers": -1 }},
+        { '$limit': 1 }  ] )
+
+    elif stat== 'anonymCitations':
+        citation = mongo.db.citations.aggregate([{ '$match': { 'author': {'$eq': None } } }, { '$group' :{  "_id" : "$author", " nbCitationSansAuteur": {'$sum': 1} } } ])
+        
+    response = dumps(citation)
+    return Response(response,
+                    status=200,
+                    mimetype='application/json')
 
 
 if __name__ == "__main__":
